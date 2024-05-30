6b4ef80f9422adfea80190a931be45d453cd8bf6
Stalin Acurio <sta_vla411@hotmail.com>
2024-05-30 09:48:38 -0500

    add nvm, docker, git

--- svam.sh
+++ svam.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
-
-
-if command -v docker &> /dev/null
-    then
-        echo "Docker ya está instalado en el sistema."
-        exit 0
-    fi
+sudo apt update -y
+sudo apt upgrade -y
 
 sudo apt install docker.io
 
 sudo apt install git-all
+
+#Install nvm for node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.zshrc
+# Cargar nvm en el entorno actual
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # Esto carga nvm
+
+# Verificar si nvm está cargado
+if command -v nvm &> /dev/null
+then
+    echo "nvm está cargado correctamente"
+else
+    echo "Error: nvm no se pudo cargar"
+    exit 1
+fi
+
+# Listar las versiones de Node.js instaladas
+nvm list
