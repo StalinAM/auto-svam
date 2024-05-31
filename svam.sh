610fda6f39ed6acb0a89e01b3559a4b30c944bf9
Stalin Acurio <sta_vla411@hotmail.com>
2024-05-31 10:36:35 -0500

    refactor

--- svam.sh
+++ svam.sh
@@ -4,11 +4,6 @@ sudo apt upgrade -y
 
 sudo apt install docker.io -y
 
-#Install nvm for node
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-
-source ~/.zshrc
-
 # install brave
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 
@@ -17,6 +12,11 @@ echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg] http
 sudo apt update -y
 
 sudo apt install brave-browser -y
+
+#Install nvm for node
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+
+source ~/.zshrc
 
 # Cargar nvm en el entorno actual
 export NVM_DIR="$HOME/.nvm"
